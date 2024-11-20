e4a85c6b68ba9d86e99e06d5f4c4bb314111fffb
Thanh Phong <ttphong444@gmail.com>
2024-11-20 22:14:24 +0700

    update footer

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,38 @@
-<footer class="d-flex flex-column align-items-center bg-darkblue text-light rounded mt-3 mx-5">
-    <h4 class="mt-2">Liên hệ</h4>
-    <p>Email: tr.thanh.phong.12a3@gmail.com</p>
-    <P>Điện thoại: 01234456678</P>
-    <p>Zalo: Thanh Phong</p>
+<footer class="text-white mt-3" style="background-color: #212936;">
+    <div class="container py-5">
+        <div class="row">
+
+            <div class="col-md-4 mb-4">
+                <h5 class="text-uppercase font-weight-bold">Về chúng tôi</h5>
+                <p class="mt-3">Cửa hàng nội thất chất lượng cao, mang đến sự tinh tế và sang trọng cho không gian
+                    sống
+                    của bạn. Cam kết chất lượng, dịch vụ tốt nhất.</p>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5 class="text-uppercase font-weight-bold">Liên kết nhanh</h5>
+                <ul class="list-unstyled mt-3">
+                    <li><a href="#" class="text-white text-decoration-none">Trang chủ</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Liên hệ</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5 class="text-uppercase font-weight-bold">Thông tin liên hệ</h5>
+                <ul class="list-unstyled mt-3">
+                    <li><i class="fas fa-map-marker-alt"></i> Địa chỉ: 123 Đường ABC, TP. XYZ</li>
+                    <li><i class="fas fa-phone"></i> Điện thoại: 0123 456 789</li>
+                    <li><i class="fas fa-envelope"></i> Email: thanhphong@noithat.com</li>
+                </ul>
+                <div class="mt-3">
+                    <a href="#" class="text-white mr-3"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white mr-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center py-3" style="background-color: #1a1f26;">
+        <p class="mb-0">© 2024 Nội thất bmazon. All Rights Reserved.</p>
+    </div>
 </footer>
